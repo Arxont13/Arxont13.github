05fe892d6ae6cc84df5ae743947e4c1d9dba97a7
Konstantin <kostya-strateg@mail.ru>
2020-12-21 18:47:53 +0300

    Etherium v1.0

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.menu-hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('menu-hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
